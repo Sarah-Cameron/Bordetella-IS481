784add438331963913b6e14d6afe5cb3236867fb
Sarah Cameron <70753510+skc33@users.noreply.github.com>
2020-09-04 16:39:25 +0100

    Update Prep Strain Tags
    Removes the need to have two separate files, contains tags upstream and downstream

--- Prep Strain Tags.R
+++ Prep Strain Tags.R
@@ -2,12 +2,9 @@
 args = commandArgs(trailingOnly = TRUE)
 #args[1]='GCF_000193595.2_ASM19359v3_genomic.fna.BLAST.Result.txt_Full_Insertions.csv'
 Insertions <- read.csv(args[1], stringsAsFactors = FALSE)
-Insertions$File <- "Full_Insertion"
 
 #Create a new table of just the strain id and the upstream and downstream location tags 
-Upstream_Tags <- Insertions[, c(3, 27, 1, 25)]
-Downstream_Tags <- Insertions[, c(3, 27, 1, 26)]
+Tags_Only <- Insertions[, c(3, 25, 26)]
 
 #Save as a new file 
-write.table(Upstream_Tags, paste(args[1],"_Upstream_Tags.csv", sep=""),row.names = F,quote = F,sep = ",")
-write.table(Downstream_Tags, paste(args[1],"_Downstream_Tags.csv", sep=""),row.names = F,quote = F,sep = ",")
+write.table(Tags_Only, paste(args[1],"_All_Tags.csv", sep=""),row.names = F,quote = F,sep = ",")
